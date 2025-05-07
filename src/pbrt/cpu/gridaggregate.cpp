cbdafe12b340dc98a48802da34c907c8935cdb31
Ccucumber12 <chris20021112@gmail.com>
2025-05-07 22:45:54 +0800

    update progress

--- src/pbrt/cpu/gridaggregate.cpp
+++ src/pbrt/cpu/gridaggregate.cpp
@@ -34,21 +34,53 @@ struct Voxel {
   }
 
 private:
-  std::vector<std::shared_ptr<Primitive>> primitives;
+  std::vector<int> primitives; // index in GridAggregate::primitives
 };
 
 
 GridAggregate::GridAggregate(std::vector<Primitive> p) : primitives(std::move(p)) {
   CHECK(!primitives.empty());
 
-  for (const auto &prim : primitives) {
+  for (const auto &prim : primitives)
     bounds = Union(bounds, prim.Bounds());
 
   Vector3f diag = bounds.pMax - bounds.pMin;
   int maxAxis = bounds.MaxDimension();
   Float invMaxWidth = 1 / diag[maxAxis];
-  CHECK(invMaxWidth > 0);
-  // TODO: Continue Here
+  CHECK_GT(invMaxWidth, 0);
+  Float cubeRoot = 3 * std::pow(Float(primitives.size()), 1 / 3.f);
+  Float voxelsPerUnitDist = cubeRoot * invMaxWidth;
+  for (int axis = 0; axis < 3; ++axis) {
+    nVoxels[axis] = pstd::round(diag[axis] * voxelsPerUnitDist);
+    nVoxels[axis] = Clamp(nVoxels[axis], 1, 64);
+  }
+
+  for (int axis = 0; axis < 3; ++axis) {
+    width[axis] = diag[axis] / nVoxels[axis];
+    invWidth[axis] = (width[axis] == 0.f) ? 0.f : 1.f / width[axis];
+  }
+  int nv = nVoxels[0] * nVoxels[1] * nVoxels[2];
+  voxels.assign(nv, Voxel());
+
+  for (uint32_t i = 0; i < primitives.size(); ++i) {
+    Bounds3f pb = primitives[i].Bounds();
+    int vmin[3], vmax[3];
+    for (int axis = 0; axis < 3; ++axis) {
+      vmin[axis] = posToVoxel(pb.pMin, axis);
+      vmax[axis] = posToVoxel(pb.pMax, axis);
+    }
+
+    for (int z = vmin[2]; z <= vmax[2]; ++z)
+      for (int y = vmin[1]; y <= vmax[1]; ++y)
+        for (int x = vmin[0]; x <= vmax[0]; ++x) {
+          int o = offset(x, y, z);
+          voxels[o].AddPrimitive(primitives[i]);
+        }
+  }
+
+
+
+  
 }
 
 
